11fe9fda9b2c01a0d8b87e3180c0e0bd220d94a4
EduardEstrada <eduard.estrada@merkle.com>
2025-07-23 17:47:50 +0200

    EduardEstrada | superMarketKata | Testing modified for new function

--- SuperMarketKata/Exercise_1/src/test/java/supermarketkata/SupermarketTest.java
+++ SuperMarketKata/Exercise_1/src/test/java/supermarketkata/SupermarketTest.java
@@ -13,7 +13,18 @@ class SupermarketTest {
 
     @Test
     public void priceSummary() {
+        PricedProduct pricedProduct = new PricedProduct("water", 10);
         PricedProduct pricedProduct = new PricedProduct("water", 20);
-        assertEquals(Supermarket.calculateSummary(List.of(pricedProduct)), 0);
+        PricedProduct pricedProduct = new PricedProduct("water", 30);
+        PricedProduct pricedProduct = new PricedProduct("water", 40);
+        PricedProduct pricedProduct = new PricedProduct("water", 50);
+        PricedProduct pricedProduct = new PricedProduct("water", 60);
+        PricedProduct pricedProduct = new PricedProduct("water", 70);
+        PricedProduct pricedProduct = new PricedProduct("water", 80);
+        PricedProduct pricedProduct = new PricedProduct("water", 90);
+        PricedProduct pricedProduct = new PricedProduct("water", 100);
+
+
+        assertEquals(Supermarket.calculateSummary(List.of(pricedProduct)), 550);
     }
 }
